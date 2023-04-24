ee9d57e96c924cba840b20d6774b360656de6e81
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-04-23 22:21:35 -0300

    Servlet básico finalizado

--- src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -10,11 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet(urlPatterns = "/oi")
+
 public class OiMundoServlet extends HttpServlet{
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
 	@Override
